7a54b822792a8fea2f82451b1480fe4191ee1d78
Daria Suntcova <batareyka23@gmail.com>
2022-10-25 17:59:15 +0300

    add Calc v1.1

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,20 @@
 import java.util.function.*;
 
-public class Calculator {
+public class Calculator extends Exception{
 
     static Supplier<Calculator> instance = Calculator::new;
 
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) -> {
+        try {
+            return x/y;
+        } catch (ArithmeticException e) {
+            System.out.println("На ноль делить нельзя!");
+            return 0;
+        }
+    };
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
